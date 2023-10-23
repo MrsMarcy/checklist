a6fff5388c5cb2d0017161f1eeb72a85be4d5a36
Manuel <126043230+MrsMarcy@users.noreply.github.com>
2023-10-23 00:48:54 -0600

    finished the edit function, checkmark function, working on title edit, and task creation

--- script.js
+++ script.js
@@ -74,9 +74,11 @@
 
 // console.log(arrTest[1].todos.splice(1,1));
 // arrTest[1].todos.forEach(element => {
-    // console.log(element);
+// console.log(element);
 // })
 // localStorage.setItem("test",lists)
+
+var html = document.body.parentNode;
 let currentList;
 let todosHtml;
 
@@ -102,17 +104,19 @@ function render(clicked_id) {
     todosHtml = '<ul id="list-group-flush" class="w-full">';
     for (const prop in chart) {
         let completed = chart[prop].completed
+        let strike = '';
         if (completed){
             completed = 'checked';
+            strike = 'strike';
         }
         todosHtml += `
-        <li class="${clicked_id} list-group-item flex" id='${todoIncrease}' onClick="hover(this)" ">
+        <li class="${clicked_id} list-group-item flex ${strike}" id='${todoIncrease}' onClick="hover(this)" >
           <div class="mx-2 my-auto">
-            <input type="checkbox" ${completed}>
+            <input onClick="check(this,${clicked_id},${todoIncrease})" type="checkbox" ${completed}>
           </div>
           <div>
             <p class="p-2" ondblClick="editText(this)">${chart[prop].text}</p>
-            <input class="editable hidden" type="text" size="100%" placeholder=" Type your item, then click the Checkmark or press ENTER" value="cheese">
+            <input id="${clicked_id}" class="${todoIncrease} editable p-2 hidden" <input onkeyDown="enter2(event, ${clicked_id}, ${todoIncrease}, value)" type="text">
           </div>
           <div class="shrink-0 flex flex-row justify-center items-center left">
             <img class="list-img cursor-pointer mx-2 edit" src="/images/pen-to-square-regular.png" alt="edit">
@@ -125,17 +129,50 @@ function render(clicked_id) {
     todosHtml += '</ul>';
     document.getElementById('current-list-todos').innerHTML = todosHtml;
 }
-
-function editText(elem) {
+html.addEventListener('click', function(event) {
+    event.preventDefault();
+    let thing = document.getElementsByClassName('editable');
+    let thing2 = document.getElementsByClassName('selected')
+    let select = thing2[0]
     let ul = document.getElementById('list-group-flush');
-    elem.nextElementSibling.value = elem.innerHTML;
-    
-    // let newLI1 = `<li class="flex">`;
-    // newLI1 += `<input class="black-box" id="input-text" type="text" size="47" placeholder=" Type your item, then click the Checkmark or press ENTER" value="${elem}">
-    // <img onClick="addFin()" class="cursor-pointer mx-2" src="/images/check-solid.png">`;
-    elem.nextElementSibling.classList.toggle('hidden');
+    // console.log(thing.length);
+    if (select){
+        if (!select.contains(event.target)) {
+            for (let i=0;i<thing.length;i++) {
+                let id = Number(thing[i].id);
+                let classN = Number(thing[i].classList[0])
+                let val = thing[i].value
+                if (val) {
+                    enter2(true, id, classN, val );
+                }   
+            }
+        }
+    }
+})
 
-    // console.log(sib);
+function check(el, id, num) {
+    if (el.checked){
+        lists[id].todos[num].completed = true;
+        el.parentNode.parentNode.classList.add('strike')
+    } else {
+        lists[id].todos[num].completed = false;
+        el.parentNode.parentNode.classList.remove('strike')
+    }
+    render(id)
+}
+function editText(elem) {
+    elem.nextElementSibling.value = elem.innerHTML;
+    elem.nextElementSibling.classList.toggle('hidden');
+    elem.nextElementSibling.classList.toggle('bg-gray')
+    elem.nextElementSibling.classList.toggle('selected')
+
+    elem.classList.toggle('hidden')
+}
+function enter2(event, id, classN, value) {
+    if (event.keyCode == 13 || event === true){
+        lists[id].todos[classN].text = value;
+        render(id)
+    }
 }
 
 function remove(example) {
@@ -154,7 +191,6 @@ function add(elem) {
     let newLI1 = `<li id="${idNum}" onClick="hover()" class="${classNum} flex py-1 my-3">`;
     newLI1 += `<input onkeyDown="enter(event,${classNum})" class="black-box " id="input-text" type="text" size="47" placeholder=" Type your item, then click the Checkmark or press ENTER" value="">
     <img onClick="addFin(${classNum})" class="cursor-pointer mx-2" src="/images/check-solid.png">`;
-    
     ul.lastElementChild.remove()
     ul.innerHTML += newLI1;
 }
